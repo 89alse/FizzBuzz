9059d1a34febeedb4160f6be288a5aeefb1b13a1
Sanela <sanela.terzic@hotmail.rs>
2019-10-27 15:05:10 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj.");
+
+            int broj = int.Parse( Console.ReadLine());
+
+            if (broj % 3 == 0 && broj % 5 != 0)
+            {
+                Console.WriteLine("Fizz :)");
+            }
+            else if (broj % 5 == 0 && broj % 3 != 0)
+            {
+                Console.WriteLine("Buzz :)");
+            }
+            else if (broj % 3 == 0 && broj % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz ;)");
+            }
+            else
+            {
+                Console.WriteLine("Tvoj broj nije zanimljiv :D");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
